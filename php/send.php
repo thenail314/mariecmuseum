e8687f295681f7479c4170b4d521235c3cdd835a
thenail314 <thenail314@gmail.com>
2021-01-07 20:57:34 +0300

    meh.

--- php/send.php
+++ php/send.php
@@ -1,35 +1,52 @@
 <?php
-/* Здесь проверяется существование переменных */
-if (isset($_POST['name'])) {$phone = $_POST['name'];}
-if (isset($_POST['phone'])) {$name = $_POST['phone'];}
- 
-/* Сюда впишите свою эл. почту */
-$myaddres  = "thenail314@yandex.ru"; // кому отправляем
- 
-/* А здесь прописывается текст сообщения, \n - перенос строки */
-$message = "Тест\nТелефон: $phone\nИмя: $name";
- 
-/* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Новый материал для музея п. Мариец'; //сабж
-$email='Музей п. Мариец'; // от кого
-$send = mail ($myaddres,$sub,$message,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
- 
-ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$subname = $_POST['subname'];
+$message = $_POST['message'];
+$showName = $_POST['showName'];
+
+$mail->isSMTP();
+$mail->Host = 'ssl://smtp.yandex.ru';
+$mail->Username = 'mariecmuseum@yandex.ru';
+$mail->Password = 'P1ssw0rD';
+$mail->SMTPAuth = true;
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+// От кого
+$mail->setFrom('mariecmuseum@yandex.ru');		
+// Кому
+$mail->addAddress('thenail314@yandex.ru');
+
+$mail->isHTML(true);
+
+$mail->Subject = 'Заявка с тестового сайта';
+$mail->Body    = '' .$name . ' ' .$subname . ' оставил заявку, его телефон ' .$phone. '<br>Сообщение: ' .$message;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+}
+
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<!DOCTYPE HTML>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="refresh" content="3; url=index.html">
-<title>Форма отправлена</title>
-<meta name="generator">
-<script type="text/javascript">
-setTimeout('location.replace("/index.php")', 3000);
-/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
-</script> 
-</head>
-<body>
-<h1>Спасибо за вклад в развитие музея!</h1>
-</body>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta http-equiv="refresh" content="3; url=index.html">
+	<title>Форма отправлена</title>
+	<meta name="generator">
+	<script type="text/javascript">
+	setTimeout('location.replace("/index.php")', 3000);
+	/*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
+	</script> 
+	</head>
+	<body>
+	<h1>Спасибо за вклад в развитие музея!</h1>
+	</body>
 </html>
