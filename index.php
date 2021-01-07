af052ac3af4e5019a4964a9734c46dc1dbb5a85f
thenail314 <thenail314@gmail.com>
2021-01-07 19:10:47 +0300

    test

--- index.php
+++ index.php
@@ -63,55 +63,50 @@
 			</ul>	
 	</div>
 
-<div class="popup" id="popup">
-	<div class="popBody">
-		<a class="popClose" href="#" style="text-decoration: none; cursor: pointer;">❌</a>
-		<div class="popContent">
-			<h2 class="popTitle">Добавить материал</h2>
-			<!-- <form action="send.php" method="post" enctype="multipart/form-data">
-				<div class="formItem">
-					<input placeholder="Имя" type="text" name="name" class="textInput" required/>
-				</div>
-
-				<div class="formItem">
-					<input placeholder="Фамилия" type="text" name="surname"  class="textInput" required/>
-				</div>
-
-				<div class="formItem">
-					<textarea placeholder="Здесь вы можете добавить текст" name="message" class="message"></textarea>
-				</div>
-
-				<div>
-					<input class="" type="file" name="photo" multiple style="margin: 10px;"/>
-				</div>
-
-				<div class="total">
-					<hr style="border: none; background-color: #333; height: 1px; width: 70%; margin: auto;">
-					<p style="margin-bottom: 15px;">Указать ваше имя на сайте?</p>
-					<div class="select">
-						<span class="choose">
-							<label>
-								<input checked type="radio" name="showName" value="showMyName"/> да</span>
-							</label>
-
-						<span class="choose">
-							<label>
-								<input type="radio" name="showName" value="hideMyName"/> нет</span>
-							</label>
+	<div class="popup" id="popup">
+		<div class="popBody">
+			<a class="popClose" href="#" style="text-decoration: none; cursor: pointer;">❌</a>
+			<div class="popContent">
+				<h2 class="popTitle">Добавить материал</h2>
+				<form action="send.php" method="post" enctype="multipart/form-data">
+					<div class="formItem">
+						<input placeholder="Имя" type="text" name="name" class="textInput" required/>
 					</div>
-					<p style="margin-top: 15px;">
-						<button class="sentForm" type="submit">Отправить</button>
-					</p>
-				</div>
-			</form> -->
-			<form action="php/send.php" method="POST">
-    <input type="text" name="name" placeholder="Ваше имя">
-    <input type="text" name="phone" placeholder="Ваш телефон">
-    <input type="submit" class="btn" >
-</form>
+
+					<div class="formItem">
+						<input placeholder="Фамилия" type="text" name="surname"  class="textInput" required/>
+					</div>
+
+					<div class="formItem">
+						<textarea placeholder="Здесь вы можете добавить текст" name="message" class="message"></textarea>
+					</div>
+
+					<div>
+						<input class="" type="file" name="photo" multiple style="margin: 10px;"/>
+					</div>
+
+					<div class="total">
+						<hr style="border: none; background-color: #333; height: 1px; width: 70%; margin: auto;">
+						<p style="margin-bottom: 15px;">Указать ваше имя на сайте?</p>
+						<div class="select">
+							<span class="choose">
+								<label>
+									<input checked type="radio" name="showName" value="showMyName"/> да</span>
+								</label>
+
+							<span class="choose">
+								<label>
+									<input type="radio" name="showName" value="hideMyName"/> нет</span>
+								</label>
+						</div>
+						<p style="margin-top: 15px;">
+							<button class="sentForm" type="submit">Отправить</button>
+						</p>
+					</div>
+				</form>
+			</div>
 		</div>
-	</div>
-</div>
+	</div>	
 
 	<div class="topContainer">
 		<img src="img/logo.png" class="logo">
--- php/send.php
+++ php/send.php
@@ -1,21 +1,20 @@
 <?php
 /* Здесь проверяется существование переменных */
 if (isset($_POST['name'])) {$phone = $_POST['name'];}
-if (isset($_POST['phone'])) {$name = $_POST['phone'];}
+if (isset($_POST['surname'])) {$name = $_POST['surname'];}
  
-/* Сюда впишите свою эл. почту */
 $myaddres  = "thenail314@yandex.ru"; // кому отправляем
  
 /* А здесь прописывается текст сообщения, \n - перенос строки */
-$message = "Тема: Заказ обратного звонка!\nТелефон: $phone\nИмя: $name";
+$message = "Имя: $name\nФамилия: $surname\nУказать имя на сайте: $showName\n\nСообщение:\n$message";
  
 /* А эта функция как раз занимается отправкой письма на указанный вами email */
-$sub='Заказ'; //сабж
-$email='Заказ обратного звонка'; // от кого
+$sub='Тема: новый материал для музея поселка Мариец'; //сабж
+$email='admin.mariecmuseum'; // от кого
 $send = mail ($myaddres,$sub,$message,"Content-type:text/plain; charset = utf-8\r\nFrom:$email");
  
 ini_set('short_open_tag', 'On');
-header('Refresh: 3; URL=index.html');
+header('Refresh: 3; URL=index.php');
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -29,7 +28,7 @@ setTimeout('location.replace("/index.php")', 3000);
 /*Изменить текущий адрес страницы через 3 секунды (3000 миллисекунд)*/
 </script> 
 </head>
-<body>
-<h1>Спасибо! Мы свяжемся с вами!</h1>
+<body style="background-color: #ddd; display: flex; align-items: center;">
+<h1 style="color: #333; text-align: center;">Спасибо! Мы свяжемся с вами!</h1>
 </body>
 </html>
