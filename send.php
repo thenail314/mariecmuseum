362a3df975637873e1370f19b96c6783971937c0
thenail314 <54327388+thenail314@users.noreply.github.com>
2021-01-09 16:20:59 +0300

    Create send.php

---
+++ send.php
@@ -0,0 +1,38 @@
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$name = $_POST['name'];
+$surname = $_POST['surname'];
+$message = $_POST['message'];
+$showName = $_POST['showName'];
+
+//$mail->SMTPDebug = 3;
+
+$mail->isSMTP();
+$mail->Host = 'smtp.yandex.ru';
+$mail->SMTPAuth = true;
+$mail->Username = 'mariecmuseum@yandex.ru';
+$mail->Password = 'P1ssw0rD';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
+
+$mail->setFrom('mariecmuseum@yandex.ru'); // отправитель
+$mail->addAddress('thenail314@yandex.ru');     // получатель 
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Вложения
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Название вложения
+$mail->isHTML(true);
+
+$mail->Subject = ''.$name . ' ' .$surname .' оставил заявку, его сообщение:';
+$mail->Body    = ''.$message.'<br><br>Показать имя: ' .$showName;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Ошибка при отправке сообщения. Попробуйте чуть позже.';
+} else {
+    header('location: thank-you.html');
+}
+
+?>
