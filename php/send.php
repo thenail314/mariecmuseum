d2f3cab8312af87885900e540da608389dc88166
thenail314 <54327388+thenail314@users.noreply.github.com>
2021-01-09 19:37:53 +0300

    Update send.php

--- php/send.php
+++ php/send.php
@@ -22,14 +22,22 @@ $mail->Port = 465;
 $mail->setFrom('mariecmuseum@yandex.ru'); // отправитель
 $mail->addAddress('thenail314@yandex.ru');     // получатель 
 $mail->isHTML(true);
-
-if(isset($_FILES['photo'])) { 
-                if($_FILES['photo']['error'] == 0){ 
-                        $mail->AddAttachment($_FILES['photo']['tmp_name'], $_FILES['attachfile']['name']); 
-                } 
-         } 
-
 $mail->Subject = ''.$name . ' ' .$surname .' оставил заявку, его сообщение:';
+
+if (!empty($_FILES['photo']['name'][0])) {
+            foreach ($_FILES['photo']['name'] as $key => $value) {
+                $out_files[] = array("name"=>$_FILES['photo']['name'][$key], "tmp_name" => $_FILES['photo']['tmp_name'][$key]);
+            }
+            $filesSend = true;
+        } else {
+            $filesSend = false;    
+        }
+        if ($filesSend) {
+            foreach ($out_files as $k=>$v) {
+                $mail->AddAttachment($out_files[$k]['tmp_name'], $out_files[$k]['name']);
+            }
+        }
+
 $mail->Body    = ''.$message.'<br><br>Показать имя: ' .$showName;
 $mail->AltBody = '';
 
