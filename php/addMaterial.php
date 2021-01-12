addcc6f997a04db36dc5d1323bbd1f416a68aed8
thenail314 <54327388+thenail314@users.noreply.github.com>
2021-01-12 21:17:18 +0300

    Update addMaterial.php

--- php/addMaterial.php
+++ php/addMaterial.php
@@ -3,9 +3,9 @@
 		<a class="popClose" href="#" style="text-decoration: none; cursor: pointer;">❌</a>
 		<div class="popContent">
 			<h2 class="popTitle">Добавить материал</h2>
-			<form action="send.php" method="POST" enctype="multipart/form-data">
+			<form action="php/send.php" method="POST" enctype="multipart/form-data">
 				<div class="formItem">
-					<input placeholder="Имя" type="text" name="firstname " class="textInput" required/>
+					<input placeholder="Имя" type="text" name="firstname" class="textInput" required/>
 				</div>
 
 				<div class="formItem">
@@ -15,24 +15,24 @@
 				<div class="formItem">
 					<textarea placeholder="Здесь вы можете добавить текст" name="message" class="message"></textarea>
 				</div>
-
 				<div>
-					<input class="" type="file" name="photo" multiple style="margin: 10px;"/>
+					<input type="file" name="photo" multiple style="margin: 9px;"/>
+					<p style="font-size: 10px; text-align: center; margin-bottom: 2px;">Прикрепите .zip архив <br> c фото или прочим материалом</p>
 				</div>
-
 				<div class="total">
 					<hr style="border: none; background-color: #333; height: 1px; width: 70%; margin: auto;">
 					<p style="margin-bottom: 15px;">Указать ваше имя на сайте?</p>
 					<div class="select">
-						<span class="choose" style="margin-right: 7px;">
+						<span class="choose" style="margin-right: 5px;">
 							<label>
 								<input checked type="radio" name="showName" value="yes"/> да
-							</label></span>
-
+							</label>
+						</span>
 						<span class="choose">
 							<label>
 								<input type="radio" name="showName" value="no"/> нет
-							</label></span>
+							</label>
+						</span>
 					</div>
 					<p style="margin-top: 15px;">
 						<button class="sentForm" type="submit">Отправить</button>
