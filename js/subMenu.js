0905e2d1e4fb15193343e9d4908da99fbe39c2cb
thenail314 <54327388+thenail314@users.noreply.github.com>
2021-01-22 20:33:09 +0300

    Delete js directory

--- js/subMenu.js
+++
@@ -1,9 +0,0 @@
-function openbox(box) {
-	display = document.getElementById('box').style.display;
-	if (display == "none") {
-		document.getElementById('box').style.display = "block";
-	} 
-	else {
-		document.getElementById('box').style.display = "none";
-	}
-}
